4aa365b27c0cd54f63cabb1e65749eb748917953
Sriket Komali <sriketk5@gmail.com>
2025-07-07 17:08:20 -0400

    fixing build w/ space

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -141,6 +141,7 @@ graph_builder = StateGraph(State)
 
 
 def get_response(state: State):
+    
     """Determine which prompt to use and get a response from the LLM.
     If the user has not answered the question, you will not provide the answer or any explanation of the answer choices.
     You can only provide the details of the patients and the context provided to you.
